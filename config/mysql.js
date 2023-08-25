ab5b963501728b25e75f240e63f48f5f0931ca7c
Naphat <naphat.d@gmail.com>
2023-08-25 14:21:59 +0700

    line bot basic 1

--- config/mysql.js
+++ config/mysql.js
@@ -2,12 +2,11 @@ const db = require('mysql2/promise')
 
 exports.connectMysql = async () => {
   const conn = await db.createConnection({
-    // host: '110.170.148.96',
-    host: '192.168.2.29',
-    user: 'root',
-    password: '1234',
-    port: '3307',
-    database: 'expressdb',
+    host: process.env.HOST_DB,
+    user: process.env.HOST_NAME,
+    password: process.env.HOST_PASSWORD,
+    port: process.env.HOST_PORT,
+    database: process.env.HOST_DB_NAME,
   })
   // const conn = await db.createConnection(process.env.DATABASE_URL)
   return conn
